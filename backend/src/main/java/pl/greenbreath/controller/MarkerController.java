ea0c7fcfba06282097a1f117687daf5052440815
parince <konrad.tupta@gmail.com>
2023-11-16 20:10:33 +0100

    Created MarkerController.java

---
+++ backend/src/main/java/pl/greenbreath/controller/MarkerController.java
@@ -0,0 +1,29 @@
+package pl.greenbreath.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pl.greenbreath.model.Marker;
+import pl.greenbreath.service.MarkerService;
+
+import java.util.List;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/marker")
+@CrossOrigin(origins = "http://localhost:63343")
+public class MarkerController {
+    private MarkerService markerService;
+
+    @PostMapping()
+    public String saveMarker(@RequestParam double lat, @RequestParam double lng) {
+        markerService.saveMarker(lat, lng);
+        return "Point saved successfully!";
+    }
+
+    @DeleteMapping()
+    public String deleteMarker(@RequestParam double lat, @RequestParam double lng) {
+        markerService.deleteMarker(lat, lng);
+        return "Point deleted successfully!";
+    }
+}
