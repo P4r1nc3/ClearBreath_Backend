8022964e2fd25102e0acbdc9e9c19d7ff43264fa
parince <konrad.tupta@gmail.com>
2023-11-16 19:09:25 +0100

    Created basic spring boot project

---
+++ backend/src/test/java/pl/greenbreath/GreenbreathApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.greenbreath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenbreathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
