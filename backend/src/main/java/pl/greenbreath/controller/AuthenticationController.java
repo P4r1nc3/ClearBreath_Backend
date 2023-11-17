60cf57cbb399f14c5317799e76a7ceddc1ec5a5a
parince <konrad.tupta@gmail.com>
2023-11-17 14:46:00 +0100

    Created AuthenticationController.java

---
+++ backend/src/main/java/pl/greenbreath/controller/AuthenticationController.java
@@ -0,0 +1,30 @@
+package pl.greenbreath.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.greenbreath.dao.request.SignUpRequest;
+import pl.greenbreath.dao.request.SignInRequest;
+import pl.greenbreath.dao.response.JwtAuthenticationResponse;
+import pl.greenbreath.service.AuthenticationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request) {
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+}
