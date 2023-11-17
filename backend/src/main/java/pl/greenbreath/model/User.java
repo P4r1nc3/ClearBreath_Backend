91004d32096da89c0315371c3521af5bde7dfe01
parince <konrad.tupta@gmail.com>
2023-11-17 18:17:18 +0100

    Remade User.java

--- backend/src/main/java/pl/greenbreath/model/User.java
+++ backend/src/main/java/pl/greenbreath/model/User.java
@@ -1,8 +1,11 @@
 package pl.greenbreath.model;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -29,6 +32,15 @@ public class User implements UserDetails {
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinTable(
+            name = "users_markers",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "marker_id")
+    )
+    private Set<Marker> markers = new HashSet<>();
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
