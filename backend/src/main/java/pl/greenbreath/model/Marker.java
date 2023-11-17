c0cc2422596370670bfb36197a59d924991720c7
parince <konrad.tupta@gmail.com>
2023-11-17 21:11:42 +0100

    Remade Marker.java

--- backend/src/main/java/pl/greenbreath/model/Marker.java
+++ backend/src/main/java/pl/greenbreath/model/Marker.java
@@ -1,23 +1,27 @@
 package pl.greenbreath.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@ToString
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "markers")
 public class Marker {
      @Id
      @GeneratedValue(strategy = GenerationType.IDENTITY)
      private Long markerId;
      private double lat;
      private double lng;
+     @JsonIgnore
+     @ManyToMany(fetch = FetchType.EAGER, mappedBy = "markers", cascade = CascadeType.MERGE)
+     private Set<User> users = new HashSet<>();
 }
