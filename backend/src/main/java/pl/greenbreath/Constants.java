a5943bbe51dc8e36a20b3b91f8f1f435effc6c9c
parince <konrad.tupta@gmail.com>
2023-11-21 20:59:46 +0100

    Updated api url

--- backend/src/main/java/pl/greenbreath/Constants.java
+++ backend/src/main/java/pl/greenbreath/Constants.java
@@ -1,5 +1,5 @@
 package pl.greenbreath;
 
 public class Constants {
-    public static final String API_URL = "https://aqicn.org";
+    public static final String API_URL = "https://api.waqi.info";
 }
