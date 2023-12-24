6010272093a4d502fb740cf1128059a48da1f740
P4r1nc3 <konrad.tupta@gmail.com>
2023-12-24 10:47:18 +0100

    Added new buttons to nav

--- frontend/scripts/authorization/auth.js
+++ frontend/scripts/authorization/auth.js
@@ -4,10 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     function updateNavButtons() {
         var navButtons = document.getElementById("navButtons");
         if (loggedIn) {
-            navButtons.innerHTML = '<li class="nav-item"><a class="nav-link" href="map.html">Map</a></li>' +
-                '<li class="nav-item"><a class="nav-link" href="#" id="logoutBtn">Logout</a></li>';
+            navButtons.innerHTML =
+                '<li class="nav-item"><a class="nav-link" href="map.html">Map</a></li>' +
+                '<li class="nav-item dropdown">' +
+                '<a class="nav-link dropdown-toggle" href="#" id="accountDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>' +
+                '<div class="dropdown-menu" aria-labelledby="accountDropdown">' +
+                '<a class="dropdown-item" href="profile.html">Profile</a>' +
+                '<a class="dropdown-item" href="#" id="logoutBtn">Logout</a>' +
+                '</div></li>';
         } else {
-            navButtons.innerHTML = '<li class="nav-item"><a class="nav-link" href="signIn.html">SignIn</a></li>' +
+            navButtons.innerHTML =
+                '<li class="nav-item"><a class="nav-link" href="signIn.html">SignIn</a></li>' +
                 '<li class="nav-item"><a class="nav-link" href="signUp.html">SignUp</a></li>';
         }
     }
