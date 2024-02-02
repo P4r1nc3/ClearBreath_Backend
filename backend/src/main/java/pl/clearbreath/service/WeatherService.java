35c7a5371f866f9f59877f3f93033142320d6bb7
parince <konrad.tupta@gmail.com>
2024-02-02 16:12:13 +0100

    Removed access modifier

--- backend/src/main/java/pl/clearbreath/service/WeatherService.java
+++ backend/src/main/java/pl/clearbreath/service/WeatherService.java
@@ -3,5 +3,5 @@ package pl.clearbreath.service;
 import pl.clearbreath.dao.response.WeatherForecastResponse;
 
 public interface WeatherService {
-    public WeatherForecastResponse getWeatherForecast(double lat, double lng);
+    WeatherForecastResponse getWeatherForecast(double lat, double lng);
 }
