03d2f2ecdebc307906c4d6c5356f0918bf3937bb
parince <konrad.tupta@gmail.com>
2024-02-03 06:46:11 +0100

    Added exception handler inside AuthenticationController.java

--- backend/src/main/java/pl/clearbreath/controller/AuthenticationController.java
+++ backend/src/main/java/pl/clearbreath/controller/AuthenticationController.java
@@ -1,14 +1,16 @@
 package pl.clearbreath.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import pl.clearbreath.ControllerUtils;
 import pl.clearbreath.dao.request.SignUpRequest;
 import pl.clearbreath.dao.request.SignInRequest;
 import pl.clearbreath.dao.response.JwtAuthenticationResponse;
+import pl.clearbreath.exception.InvalidLoginDetailsException;
+import pl.clearbreath.exception.UserAlreadyExistsException;
 import pl.clearbreath.service.AuthenticationService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,5 +28,25 @@ public class AuthenticationController {
     @PostMapping("/signin")
     public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request) {
         return ResponseEntity.ok(authenticationService.signin(request));
+    }
+
+    @ExceptionHandler(InvalidLoginDetailsException.class)
+    public ResponseEntity<Object> handleInvalidLoginDetailsException(InvalidLoginDetailsException ex, HttpServletRequest request) {
+        return ControllerUtils.createErrorResponse(
+                HttpStatus.UNAUTHORIZED,
+                ex.getMessage(),
+                "Check your login credentials and try again.",
+                request
+        );
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Object> handleUserAlreadyExistsException(UserAlreadyExistsException ex, HttpServletRequest request) {
+        return ControllerUtils.createErrorResponse(
+                HttpStatus.CONFLICT,
+                ex.getMessage(),
+                "Try using a different email.",
+                request
+        );
     }
 }
