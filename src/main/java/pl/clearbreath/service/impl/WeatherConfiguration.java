148d2c37b581962f27d1fb090dc89fa254eff333
P4r1nc3 <konrad.tupta@gmail.com>
2024-10-20 09:52:05 +0200

    Fixed weather caching

--- src/main/java/pl/clearbreath/service/impl/WeatherConfiguration.java
+++ src/main/java/pl/clearbreath/service/impl/WeatherConfiguration.java
@@ -2,6 +2,7 @@ package pl.clearbreath.service.impl;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -12,8 +13,12 @@ public class WeatherConfiguration {
 
     @Primary
     @Bean(name = "cached-weather-service")
-    public WeatherService cachedWeatherService(@Qualifier("weather-service") WeatherService weatherService, MeterRegistry meterRegistry) {
-        return new CachedWeatherServiceImpl(weatherService, meterRegistry);
+    public WeatherService cachedWeatherService(
+            @Qualifier("weather-service") WeatherService weatherService,
+            MeterRegistry meterRegistry,
+            @Value("${weather.cache.expire-after-write-minutes}") long expireAfterWriteMinutes,
+            @Value("${weather.cache.max-size}") long maxSize) {
+        return new CachedWeatherServiceImpl(weatherService, meterRegistry, expireAfterWriteMinutes, maxSize);
     }
 
     @Bean(name = "weather-service")
