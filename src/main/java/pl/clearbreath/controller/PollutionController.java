a3ee1d91638bdab1c2a5e5c51a83a6d6888b127d
parince <konrad.tupta@gmail.com>
2024-10-29 12:44:14 +0100

    Improved PollutionController.java

--- src/main/java/pl/clearbreath/controller/PollutionController.java
+++ src/main/java/pl/clearbreath/controller/PollutionController.java
@@ -1,19 +1,23 @@
 package pl.clearbreath.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import pl.clearbreath.dao.response.AirQualityResponse;
 import pl.clearbreath.service.PollutionService;
 
+@Slf4j
 @RestController
-@AllArgsConstructor
 @RequestMapping("/pollution")
 public class PollutionController {
-
     @Qualifier("cached-pollution-service")
     private final PollutionService pollutionService;
+
+    public PollutionController(PollutionService pollutionService) {
+        this.pollutionService = pollutionService;
+    }
 
     @GetMapping("/lat/{lat}/lng/{lng}")
     public AirQualityResponse getMarker(@PathVariable double lat, @PathVariable double lng) {
