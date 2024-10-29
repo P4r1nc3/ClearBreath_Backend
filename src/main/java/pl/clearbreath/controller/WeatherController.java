65b8e7ec4682ae1ccbdfd1f424ba22cef56fa0fc
parince <konrad.tupta@gmail.com>
2024-10-29 12:44:30 +0100

    Improved WeatherController.java

--- src/main/java/pl/clearbreath/controller/WeatherController.java
+++ src/main/java/pl/clearbreath/controller/WeatherController.java
@@ -1,17 +1,21 @@
 package pl.clearbreath.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 import pl.clearbreath.dao.response.WeatherForecastResponse;
 import pl.clearbreath.service.WeatherService;
 
+@Slf4j
 @RestController
-@AllArgsConstructor
 @RequestMapping("/weather")
 public class WeatherController {
     @Qualifier("cached-weather-service")
     private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
 
     @GetMapping("/lat/{lat}/lng/{lng}")
     public WeatherForecastResponse getMarkerForecast(@PathVariable double lat, @PathVariable double lng) {
