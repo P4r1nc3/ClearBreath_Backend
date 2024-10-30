c88ee52a8ac821309ef4e24c0f774c900c2e6230
parince <konrad.tupta@gmail.com>
2024-10-30 10:12:42 +0100

    Removed unused import

--- src/main/java/pl/clearbreath/controller/WeatherController.java
+++ src/main/java/pl/clearbreath/controller/WeatherController.java
@@ -3,7 +3,6 @@ package pl.clearbreath.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.clearbreath.dao.response.WeatherForecastResponse;
